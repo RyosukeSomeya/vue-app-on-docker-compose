ee82e1cbe84bf2ced40dddcf11c5fd6bcdfe8b6b
RyosukeSomeya <rx.78.0618@gmail.com>
2021-07-17 23:55:14 +0900

    TodoDisplay:静的表示テスト追加

--- vue-todo-list/src/components/__tests__/TodoDisplay.spec.js
+++ vue-todo-list/src/components/__tests__/TodoDisplay.spec.js
@@ -2,8 +2,16 @@ import { mount } from '@vue/test-utils';
 import TodoDisplay from '@/components/TodoDisplay';
 
 describe('Testing TodoDisplay component', () => {
-    it('Testing TodoList component', () => {
-        const wrapper = mount(TodoDisplay)
-        expect(wrapper.isVueInstance).toBeTruthy()
-    })
+    // コンポーネント全体の確認
+    const wrapper = mount(TodoDisplay);
+    it('コンポーネントが表示されているか確認', () => {
+        console.log(wrapper.html())
+        expect(wrapper.isVueInstance).toBeTruthy();
+        expect(wrapper.findComponent(TodoDisplay).exists()).toBe(true);
+    });
+
+    it('tableのtheadが表示されているか確認', () => {
+        // expect(wrapper.findComponent('thead')).toBe(true);
+        // expect(wrapper.html().classes()).toContain('todo-it');
+    });
 })
