1d1feb029d6bf77b70e13e22239cdc2c35d611e7
RyosukeSomeya <rx.78.0618@gmail.com>
2021-07-18 00:25:30 +0900

    TodoDisplay:初期表示確認テスト実装

--- vue-todo-list/src/components/__tests__/TodoDisplay.spec.js
+++ vue-todo-list/src/components/__tests__/TodoDisplay.spec.js
@@ -1,17 +1,22 @@
-import { mount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import TodoDisplay from '@/components/TodoDisplay';
 
 describe('Testing TodoDisplay component', () => {
     // コンポーネント全体の確認
-    const wrapper = mount(TodoDisplay);
+    const wrapper = shallowMount(TodoDisplay);
     it('コンポーネントが表示されているか確認', () => {
-        console.log(wrapper.html())
         expect(wrapper.isVueInstance).toBeTruthy();
         expect(wrapper.findComponent(TodoDisplay).exists()).toBe(true);
     });
 
+    // 初期表示の確認
     it('tableのtheadが表示されているか確認', () => {
-        // expect(wrapper.findComponent('thead')).toBe(true);
-        // expect(wrapper.html().classes()).toContain('todo-it');
+        expect(wrapper.find('thead').exists()).toBeTruthy();
+        // thは4つ存在する
+        expect(wrapper.findAll('th').length).toEqual(4);
+        // クラス名ｓ'todo-it'と'comment-col'の
+        expect(wrapper.find('.todo-it').exists()).toBeTruthy();
+        expect(wrapper.find('.comment-col').exists()).toBeTruthy();
     });
+
 })
